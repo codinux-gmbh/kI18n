6a07318b3cca58c2af5a960d5287d2c1cd2c56e0
dankito <git@dankito.net>
2024-12-18 12:40:15 +0100

    Added default region and script to Language

--- CldrParser/src/main/kotlin/net/codinux/i18n/codegenerator/LanguageEnumGenerator.kt
+++ CldrParser/src/main/kotlin/net/codinux/i18n/codegenerator/LanguageEnumGenerator.kt
@@ -3,8 +3,11 @@ package net.codinux.i18n.codegenerator
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.TypeSpec
 import net.codinux.i18n.LanguageTag
+import net.codinux.i18n.Region
+import net.codinux.i18n.Script
 import net.codinux.i18n.model.LanguageDisplayNames
 import net.codinux.i18n.parser.CldrJsonParser
+import net.codinux.i18n.service.LikelySubtagsCategorizer
 
 class LanguageEnumGenerator(
     private val cldrJsonParser: CldrJsonParser = CldrJsonParser(),
@@ -17,18 +20,26 @@ class LanguageEnumGenerator(
         // there are some languages for that no English name has been specified, also add these:
         englishLanguageNames.addAll(getLanguageCodesForWhichNoEnglishNameHasBeenSet(englishLanguageNames))
 
+        val defaultRegionsAndScripts = LikelySubtagsCategorizer(cldrJsonParser).getDefaultScriptAndRegionForAllLanguages()
+
         val constructor = FunSpec.constructorBuilder()
             .addParameter("isoCode", String::class, false, "Lowercase alpha-2 two-letter or alpha-3 three-letter ISO 639 language code.")
             .addParameter("englishName", String::class, false, "English name of the language.")
             .addParameter("localName", String::class, true, "The endonym as speakers of this language refer themselves to their language.")
+            .addParameter("defaultRegion", Region::class, true, "Language's default region.")
+            .addParameter("defaultScript", Script::class, true, "Language's default script (writing system).")
             .build()
 
 
         val enumConstants = englishLanguageNames.sortedBy { it.languageIsoCode }.map { language ->
+            val (region, script) = defaultRegionsAndScripts[language.languageIsoCode] ?: (null to null)
+
             language.displayName to TypeSpec.anonymousClassBuilder()
                 .addSuperclassConstructorParameter("%S", language.languageIsoCode)
                 .addSuperclassConstructorParameter("%S", language.displayName)
                 .addNullableSuperclassConstructorParameter(getEndonym(language))
+                .addNullableSuperclassConstructorParameter(region)
+                .addNullableSuperclassConstructorParameter(script)
                 .build()
         }
 
