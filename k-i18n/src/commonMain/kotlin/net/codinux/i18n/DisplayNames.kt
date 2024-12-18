51085fe889c57dc6338bb09bcd56b587695220d5
dankito <git@dankito.net>
2024-12-18 15:09:24 +0100

    Extracted getDisplayNameHierarchically()

--- k-i18n/src/commonMain/kotlin/net/codinux/i18n/DisplayNames.kt
+++ k-i18n/src/commonMain/kotlin/net/codinux/i18n/DisplayNames.kt
@@ -5,109 +5,53 @@ import kotlin.jvm.JvmOverloads
 open class DisplayNames {
 
     @JvmOverloads
-    open fun getAllLanguageDisplayNamesForLanguage(language: LanguageTag = LanguageTag.current): Map<String, String>? {
-        val displayNames = LanguageDisplayNames.getDisplayNamesForLocale(language)
-
-        return if (displayNames.isNullOrEmpty() == false) {
-            displayNames
-        } else {
-            val parent = language.parent
-            if (parent != null) {
-                getAllLanguageDisplayNamesForLanguage(parent)
-            } else {
-                null
-            }
-        }
-    }
+    open fun getAllLanguageDisplayNamesForLanguage(language: LanguageTag = LanguageTag.current): Map<String, String>? =
+        getDisplayNameHierarchically(language) { LanguageDisplayNames.getDisplayNamesForLocale(it) }
 
     @JvmOverloads
     open fun getLanguageDisplayName(language: Language, displayLanguage: LanguageTag = LanguageTag.current): String? =
         getLanguageDisplayName(language.isoCode, displayLanguage)
 
     @JvmOverloads
-    open fun getLanguageDisplayName(languageIsoCode: String, language: LanguageTag = LanguageTag.current): String? {
-        val displayName = LanguageDisplayNames.getDisplayName(languageIsoCode, language)
-
-        return if (displayName != null) {
-            displayName
-        } else {
-            val parent = language.parent
-            if (parent != null) {
-                getLanguageDisplayName(languageIsoCode, parent)
-            } else {
-                null
-            }
-        }
-    }
+    open fun getLanguageDisplayName(languageIsoCode: String, language: LanguageTag = LanguageTag.current): String? =
+        getDisplayNameHierarchically(language) { LanguageDisplayNames.getDisplayName(languageIsoCode, it) }
 
 
     @JvmOverloads
-    open fun getAllRegionDisplayNamesForLanguage(language: LanguageTag = LanguageTag.current): Map<String, String>? {
-        val displayNames = RegionDisplayNames.getDisplayNamesForLocale(language)
-
-        return if (displayNames.isNullOrEmpty() == false) {
-            displayNames
-        } else {
-            val parent = language.parent
-            if (parent != null) {
-                getAllRegionDisplayNamesForLanguage(parent)
-            } else {
-                null
-            }
-        }
-    }
+    open fun getAllRegionDisplayNamesForLanguage(language: LanguageTag = LanguageTag.current): Map<String, String>? =
+        getDisplayNameHierarchically(language) { RegionDisplayNames.getDisplayNamesForLocale(it) }
 
     @JvmOverloads
     open fun getRegionDisplayName(region: Region, language: LanguageTag = LanguageTag.current): String? =
         getRegionDisplayName(region.code, language)
 
     @JvmOverloads
-    open fun getRegionDisplayName(regionCode: String, language: LanguageTag = LanguageTag.current): String? {
-        val displayName = RegionDisplayNames.getDisplayName(regionCode, language)
-
-        return if (displayName != null) {
-            displayName
-        } else {
-            val parent = language.parent
-            if (parent != null) {
-                getRegionDisplayName(regionCode, parent)
-            } else {
-                null
-            }
-        }
-    }
+    open fun getRegionDisplayName(regionCode: String, language: LanguageTag = LanguageTag.current): String? =
+        getDisplayNameHierarchically(language) { RegionDisplayNames.getDisplayName(regionCode, it) }
 
 
     @JvmOverloads
-    open fun getAllCurrencyDisplayNamesForLanguage(language: LanguageTag = LanguageTag.current): Map<String, String>? {
-        val displayNames = CurrencyDisplayNames.getDisplayNamesForLocale(language)
-
-        return if (displayNames.isNullOrEmpty() == false) {
-            displayNames
-        } else {
-            val parent = language.parent
-            if (parent != null) {
-                getAllCurrencyDisplayNamesForLanguage(parent)
-            } else {
-                null
-            }
-        }
-    }
+    open fun getAllCurrencyDisplayNamesForLanguage(language: LanguageTag = LanguageTag.current): Map<String, String>? =
+        getDisplayNameHierarchically(language) { CurrencyDisplayNames.getDisplayNamesForLocale(it) }
 
     @JvmOverloads
     open fun getCurrencyDisplayName(currency: Currency, language: LanguageTag = LanguageTag.current): String? =
         getCurrencyDisplayName(currency, language)
 
     @JvmOverloads
-    open fun getCurrencyDisplayName(currencyIsoCode: String, language: LanguageTag = LanguageTag.current): String? {
-        val displayName = CurrencyDisplayNames.getDisplayName(currencyIsoCode, language)
+    open fun getCurrencyDisplayName(currencyIsoCode: String, language: LanguageTag = LanguageTag.current): String? =
+        getDisplayNameHierarchically(language) { CurrencyDisplayNames.getDisplayName(currencyIsoCode, it) }
 
-        return if (displayName != null) {
+
+    protected open fun <T> getDisplayNameHierarchically(language: LanguageTag, getForTag: (LanguageTag) -> T?): T? {
+        val displayName = getForTag(language)
+
+        return if (displayName != null && (displayName !is Map<*, *> || displayName.isNotEmpty())) {
             displayName
         } else {
             val parent = language.parent
             if (parent != null) {
-                getCurrencyDisplayName(currencyIsoCode, parent)
+                getForTag(parent)
             } else {
                 null
             }
