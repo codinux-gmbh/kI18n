480244a841692e6f8f732c669deb7d6a7e6dc920
dankito <git@dankito.net>
2024-12-18 13:13:06 +0100

    Added default language region to Script

--- CldrParser/src/main/kotlin/net/codinux/i18n/codegenerator/ScriptEnumGenerator.kt
+++ CldrParser/src/main/kotlin/net/codinux/i18n/codegenerator/ScriptEnumGenerator.kt
@@ -2,11 +2,13 @@ package net.codinux.i18n.codegenerator
 
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.TypeSpec
+import net.codinux.i18n.Language
 import net.codinux.i18n.LanguageTag
 import net.codinux.i18n.Region
 import net.codinux.i18n.model.ScriptDisplayName
 import net.codinux.i18n.model.ScriptMetadata
 import net.codinux.i18n.parser.CldrJsonParser
+import net.codinux.i18n.service.LikelySubtagsCategorizer
 
 class ScriptEnumGenerator(
     private val cldrJsonParser: CldrJsonParser = CldrJsonParser(),
@@ -18,28 +20,36 @@ class ScriptEnumGenerator(
 
         val scriptMetadata = cldrJsonParser.parseScriptsMetadata()
 
+        val defaultLanguagesAndRegions = LikelySubtagsCategorizer(cldrJsonParser).getDefaultLanguageAndRegionForAllScripts()
+            .mapKeys { it.key.code }
+
         val constructor = FunSpec.constructorBuilder()
             .addParameter("code", String::class, false, IsoCodeKdoc)
             .addParameter("englishName", String::class, false, "English name of the script.")
             .addParameter("variantName", String::class, true, "Optional a variant of the English name of the script (if available).")
+            .addParameter("defaultLanguage", Language::class, true, "Script's default language.")
+            .addParameter("defaultRegion", Region::class, true, "Script's default region.")
             .addParameter("originCountry", Region::class, true, "The origin region of the script, if available.")
             .build()
 
 
         val enumConstants = englishScriptNames.map { scriptName ->
-            createEnumConstant(scriptName, scriptMetadata[scriptName.code])
+            createEnumConstant(scriptName, scriptMetadata[scriptName.code], defaultLanguagesAndRegions[scriptName.code])
         }
 
         util.writeEnumClass("Script", enumConstants, constructor, "Defines all Scripts (writing systems) known to CLDR.")
     }
 
-    private fun createEnumConstant(scriptName: ScriptDisplayName, scriptMetadata: ScriptMetadata?): Pair<String, TypeSpec> {
+    private fun createEnumConstant(scriptName: ScriptDisplayName, scriptMetadata: ScriptMetadata?,
+                                   defaultLanguageAndScript: Pair<Language?, Region>?): Pair<String, TypeSpec> {
         val code = scriptName.code
 
         return scriptName.displayName to TypeSpec.anonymousClassBuilder()
             .addSuperclassConstructorParameter("%S", code)
             .addNullableSuperclassConstructorParameter(scriptName.displayName)
             .addNullableSuperclassConstructorParameter(scriptName.shortDisplayName ?: scriptName.variantDisplayName)
+            .addNullableSuperclassConstructorParameter(defaultLanguageAndScript?.first)
+            .addNullableSuperclassConstructorParameter(defaultLanguageAndScript?.second)
             .addNullableSuperclassConstructorParameter(scriptMetadata?.originCountry)
             // TODO: may also add numeric ISO code from ISO 15924.csv
             .build()
