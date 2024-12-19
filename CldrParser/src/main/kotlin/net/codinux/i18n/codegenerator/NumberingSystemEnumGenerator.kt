3e691e12031b5246f988e9c2de3afce2204c7619
dankito <git@dankito.net>
2024-12-19 04:01:09 +0100

    Added englishName to LocaleDisplayName

--- CldrParser/src/main/kotlin/net/codinux/i18n/codegenerator/NumberingSystemEnumGenerator.kt
+++ CldrParser/src/main/kotlin/net/codinux/i18n/codegenerator/NumberingSystemEnumGenerator.kt
@@ -2,13 +2,10 @@ package net.codinux.i18n.codegenerator
 
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.TypeSpec
+import net.codinux.i18n.LanguageTag
 import net.codinux.i18n.NumberingSystemType
 import net.codinux.i18n.model.NumberingSystem
 import net.codinux.i18n.parser.CldrJsonParser
-
-fun main() {
-    NumberingSystemEnumGenerator().generate()
-}
 
 class NumberingSystemEnumGenerator(
     private val cldrJsonParser: CldrJsonParser = CldrJsonParser(),
@@ -18,27 +15,32 @@ class NumberingSystemEnumGenerator(
     fun generate() {
         val numberingSystems = cldrJsonParser.parseNumberingSystems()
 
+        val englishNames = cldrJsonParser.parseLocaleDisplayNamesForLocale(LanguageTag.English)
+        val englishNamesByCode = englishNames.numberingSystems
+
         val constructor = FunSpec.constructorBuilder()
-            .addParameter("code", String::class, false, "The four-letter lowercase code of this numbering system.")
+            .addParameter("code", String::class, false, "The lowercase code of this numbering system.")
             .addParameter("type", NumberingSystemType::class, false, "The type of this numbering system (numeric or algorithmic).")
             .addParameter("digits", String::class, true, "The digits that this numbering system has.")
             .addParameter("rules", String::class, true)
+            .addParameter("englishName", String::class, true)
             .build()
 
 
         val enumConstants = numberingSystems.map { numberingSystem ->
-            createEnumConstant(numberingSystem)
-        }
+            createEnumConstant(numberingSystem, englishNamesByCode[numberingSystem.code])
+        }.sortedBy { it.first } // sort by display name
 
         util.writeEnumClass("NumberingSystem", enumConstants, constructor, "Defines all Numbering Systems known to CLDR.")
     }
 
-    private fun createEnumConstant(numberingSystem: NumberingSystem): Pair<String, TypeSpec> {
-        return numberingSystem.code to TypeSpec.anonymousClassBuilder()
+    private fun createEnumConstant(numberingSystem: NumberingSystem, displayName: String?): Pair<String, TypeSpec> {
+        return (displayName ?: numberingSystem.code) to TypeSpec.anonymousClassBuilder()
             .addSuperclassConstructorParameter("%S", numberingSystem.code)
             .addNullableSuperclassConstructorParameter(numberingSystem.type)
             .addNullableSuperclassConstructorParameter(numberingSystem.digits)
             .addNullableSuperclassConstructorParameter(numberingSystem.rules)
+            .addNullableSuperclassConstructorParameter(displayName)
             .build()
     }
 
